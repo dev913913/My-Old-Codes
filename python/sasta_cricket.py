5dcb92f6c3ee46408fe37621ea4b9f392401a03d
codingwithDev <165135910+dev913913@users.noreply.github.com>
2024-10-24 09:06:31 +0000

    Encapsulated the game code in a function. Also added __name__ = "__main__"

--- python/sasta_cricket.py
+++ python/sasta_cricket.py
@@ -66,60 +66,62 @@ def user_bat(comp_score, user_score):
             print(f"Your current score: {user_score}")
     return user_score        
         
+def sastaCricket():
+    while True: 
+        print("Welcome to the Sasta Cricket Game!\n")
+        print("Simple Rules:\n")
+        print("  1. Choose numbers between 0 and 6.")
+        print("  2. Bowler tries to match their number with the batsman's.")
+        print("  3. If they match, the batsman is out.")
+        print("  4. If not, the batsman's number adds to their score.")
+        print("  5. Players swap roles after a match.")
+        print("  6. The new batsman tries to beat the previous score.")
 
-while True: 
-    print("Welcome to the Sasta Cricket Game!\n")
-    print("Simple Rules:\n")
-    print("  1. Choose numbers between 0 and 6.")
-    print("  2. Bowler tries to match their number with the batsman's.")
-    print("  3. If they match, the batsman is out.")
-    print("  4. If not, the batsman's number adds to their score.")
-    print("  5. Players swap roles after a match.")
-    print("  6. The new batsman tries to beat the previous score.")
+        ch1 = input("\nEnter 'Y' to continue: ")
 
-    ch1 = input("\nEnter 'Y' to continue: ")
-
-    if ch1.lower() != "y":
-       break
-   
-    comp_score = 0
-    user_score = 0
+        if ch1.lower() != "y":
+            break
     
-    print("\nWhat would you like to do first: \n")
-    print(" [1] Batting")
-    print(" [2] Bowling\n")
-    ch2 = int(input("Enter 1 or 2: "))
-    uf = 0
-    match ch2:
-        case 1:
+        comp_score = 0
+        user_score = 0
+        
+        print("\nWhat would you like to do first: \n")
+        print(" [1] Batting")
+        print(" [2] Bowling\n")
+        ch2 = int(input("Enter 1 or 2: "))
+        uf = 0
+        match ch2:
+            case 1:
+                user_score = user_bat(comp_score, user_score)
+                uf += 1    
+            case 2:
+                comp_score = comp_bat(comp_score, user_score) 
+            case _:
+                print("Invalid Choice!")
+        if uf > 0:
+            print(f"Bowl out Computer before he scores more than {user_score}")
+            comp_score = comp_bat(comp_score, user_score)
+            if comp_score > user_score:
+                print(f"Hehe! Computer has won with extra {comp_score-user_score} runs")
+            elif comp_score == user_score:
+                print("It's a draw!") 
+            else: print(f"Congratulations! You have won with extra {user_score-comp_score} runs")    
+        else: 
+            print(f"Your target is to score more than {comp_score}")
             user_score = user_bat(comp_score, user_score)
-            uf += 1    
-        case 2:
-            comp_score = comp_bat(comp_score, user_score) 
-        case _:
-            print("Invalid Choice!")
-    if uf > 0:
-        print(f"Bowl out Computer before he scores more than {user_score}")
-        comp_score = comp_bat(comp_score, user_score)
-        if comp_score > user_score:
-            print(f"Hehe! Computer has won with extra {comp_score-user_score} runs")
-        elif comp_score == user_score:
-            print("It's a draw!") 
-        else: print(f"Congratulations! You have won with extra {user_score-comp_score} runs")    
-    else: 
-        print(f"Your target is to score more than {comp_score}")
-        user_score = user_bat(comp_score, user_score)
-        if comp_score > user_score:
-            print(f"Hehe! Computer has won with extra {comp_score-user_score} runs")
-        elif comp_score == user_score:
-            print("It's a draw!") 
-        else: print(f"Congratulations! You have won with extra {user_score-comp_score} runs")
-        
-    ch3 =  input("\nDo you want to play again? (Y/N): ").strip().lower()
-    if ch3!= "y":
-        print("")
-        break
-        
+            if comp_score > user_score:
+                print(f"Hehe! Computer has won with extra {comp_score-user_score} runs")
+            elif comp_score == user_score:
+                print("It's a draw!") 
+            else: print(f"Congratulations! You have won with extra {user_score-comp_score} runs")
+            
+        ch3 =  input("\nDo you want to play again? (Y/N): ").strip().lower()
+        if ch3!= "y":
+            print("")
+            break
+    
+if __name__ == "__main__":
+    sastaCricket()
 
     
         
