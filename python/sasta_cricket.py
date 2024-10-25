ea9fe8f0b3b58fa3ad2dbd24ef935511b6929c8e
codingwithDev <165135910+dev913913@users.noreply.github.com>
2024-10-25 09:58:29 +0000

    MODIFIED SO THAT MATCH WILL BE OVER RIGHT AFTE one player has scored more than other player's score. Instead of waiting to get out first

--- python/sasta_cricket.py
+++ python/sasta_cricket.py
@@ -20,10 +20,20 @@ def print_lbyl(text, delay=0.07):
         
 def verify(n):
     return 0 <= n <= 6
+
+def compare_score(comp_score, user_score):
+    if comp_score > user_score:
+        print(f"\nHehe! Computer has won with extra {comp_score-user_score} runs")
+    elif comp_score == user_score:
+        print("\nIt's a draw!") 
+    else: print(f"\nCongratulations! You have won with extra {user_score-comp_score} runs") 
+            
+def comp_bat(comp_score, user_score, uf, cf):
+    
+    if uf == 1:
+        print(f"Bowl out computer before it scores more than '{user_score}'")
         
-    
-    
-def comp_bat(comp_score, user_score):
+    # print("\nComputer's turn")
     print("\n")
     print_lbyl("Bowl: ")
     while True:
@@ -43,9 +53,14 @@ def comp_bat(comp_score, user_score):
                 sleep(1)
                 print(f"Computer is out at {comp_score}")
                 sleep(1)
-                break
-            # elif comp_score > user_score:
-            #     compiler_score += comp_hand
+                
+                if uf == 1:
+                    compare_score(comp_score, user_score)
+                    break
+                else: 
+                    user_bat(comp_score, user_score, uf, cf)
+                    break 
+                    
             else:
                 comp_score += comp_hand
                 sleep(0.6)
@@ -64,7 +79,11 @@ def comp_bat(comp_score, user_score):
             print_lbyl("Bowl: ") 
     return comp_score
     
-def user_bat(comp_score, user_score):
+def user_bat(comp_score, user_score, uf, cf):
+    
+    if cf == 1:
+        print(f"Your target is to score more than '{comp_score}'")
+    
     print("\n")
     print_lbyl("Hit: ")
     while True:
@@ -85,7 +104,15 @@ def user_bat(comp_score, user_score):
                 sleep(1)        
                 print(f"You're out at {user_score}")
                 sleep(1)
-                break
+                
+                if cf == 1:
+                    compare_score(comp_score, user_score)
+                    break
+                else: 
+                    comp_bat(comp_score, user_score, uf, cf)
+                    break 
+            
+            
             else:
                 user_score += user_hand
                 sleep(0.6)
@@ -137,35 +164,31 @@ def sastaCricket():
         print(" [1] Batting")
         print(" [2] Bowling\n")
         
-        uf = 0
+
         while True:
             ch2 = input("Enter 1 or 2: ")
             match ch2:
                 case '1':
-                    user_score = user_bat(comp_score, user_score)
-                    uf += 1
+                    user_bat(comp_score, user_score,1, 0)
                     break    
                 case '2':
-                    comp_score = comp_bat(comp_score, user_score)
+                    comp_bat(comp_score, user_score,0,1)
                     break 
                 case _:
                     print("\nInvalid Choice!\n")
-        if uf > 0:
-            print(f"\nBowl out Computer before he scores more than {user_score}")
-            comp_score = comp_bat(comp_score, user_score)
-            if comp_score > user_score:
-                print(f"\nHehe! Computer has won with extra {comp_score-user_score} runs")
-            elif comp_score == user_score:
-                print("\nIt's a draw!") 
-            else: print(f"\nCongratulations! You have won with extra {user_score-comp_score} runs")    
-        else: 
-            print(f"\nYour target is to score more than {comp_score}")
-            user_score = user_bat(comp_score, user_score)
-            if comp_score > user_score:
-                print(f"\nHehe! Computer has won with extra {comp_score-user_score} runs")
-            elif comp_score == user_score:
-                print("It's a draw!") 
-            else: print(f"\nCongratulations! You have won with extra {user_score-comp_score} runs")
+                    
+        # if uf > 0:
+        #     print(f"\nBowl out Computer before he scores more than {user_score}")
+        #     comp_bat(comp_score, user_score)
+               
+        # else: 
+        #     print(f"\nYour target is to score more than {comp_score}")
+        #     user_score = user_bat(comp_score, user_score)
+        #     if comp_score > user_score:
+        #         print(f"\nHehe! Computer has won with extra {comp_score-user_score} runs")
+        #     elif comp_score == user_score:
+        #         print("It's a draw!") 
+        #     else: print(f"\nCongratulations! You have won with extra {user_score-comp_score} runs")
             
         ch3 =  input("\nDo you want to play again? (Y/N): ").strip().lower()
         if ch3!= "y":
